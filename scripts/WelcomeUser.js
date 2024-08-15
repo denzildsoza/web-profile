7834dc507477d5b19f3713ebff5b89a1f6e06fd7
denzildsoza <denzildsouza3661@gmail.com>
2024-08-15 13:31:36 +0530

    skills grid

--- scripts/WelcomeUser.js
+++ scripts/WelcomeUser.js
@@ -1,10 +1,18 @@
 const welcomeUser = {
   welcomeText: "Namaste",
-  delay:100,
-  welcomeClient($el, welcomeText,delay=this.delay) {
+  userWelcome: false,
+  delay: 100,
+  setUserWelcome() {
+    this.userWelcome = true;
+  },
+  welcomeClient($el, welcomeText, delay = this.delay) {
     for (let i in welcomeText) {
       setTimeout(() => ($el.textContent += welcomeText[i]), delay + i * delay);
     }
+    const welcomeDiv = document.getElementById("welcome-page-content");
+    setTimeout(() => {
+      welcomeDiv.setAttribute("class", "transform-Out-Of-View");
+    }, delay*2 + welcomeText.length * delay);
   },
 };
 
--- scripts/navBar.js
+++ scripts/navBar.js
@@ -1,7 +1,38 @@
 const navBar = {
-  pageActive: "",
-  pages: ["profile", "skills", "experience", "education", "contact"],
-  
+  pageActive: "home",
+  pages: [
+    {
+      id: "homepage-section",
+      name: "home",
+    },
+    {
+      id: "about-section",
+      name: "profile",
+    },
+    {
+      id: "skills-section",
+      name: "skills",
+    },
+    {
+      id: "experience-section",
+      name: "experience",
+    },
+    {
+      id: "education-section",
+      name: "education",
+    },
+    {
+      id: "contact-section",
+      name: "contact",
+    },
+  ],
+
+
+  scrollIntoView($el, id) {
+    const pageToScroll = document.getElementById(id);
+    pageToScroll.scrollIntoView({ behavior: "smooth" });
+  },
+
   activePageClass(item) {
     if (item !== this.pageActive) return "";
     return "pageActive";
@@ -17,4 +48,4 @@ const navBar = {
   },
 };
 
-export default navBar
+export default navBar;
